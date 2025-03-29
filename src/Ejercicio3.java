613f74165b31c6d41d19cea439fd76cd14ea612d
RobinsonLatour <latourrobinson@gmail.com>
2025-03-29 11:58:55 -0300

    Primer Commit

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese el año actual");
+        int actual = leer.nextInt();
+
+        System.out.println("Ingrese su año de nacimiento");
+        int nacimiento = leer.nextInt();
+
+        System.out.println("Su edad es: " + (actual - nacimiento) + " años");
 
     }
 }
