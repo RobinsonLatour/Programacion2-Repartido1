2e6e495ea01e798fed52abdff8ea388f64d527cd
RobinsonLatour <latourrobinson@gmail.com>
2025-03-29 11:39:46 -0300

    Primer Commit

---
+++ src/Ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número entero");
+        int num1 = leer.nextInt();
+
+        System.out.println("Ingrese el segundo número entero");
+        int num2 = leer.nextInt();
+
+        System.out.println("El resultado de la suma es: " + (num1 + num2));
+
+    }
+}
