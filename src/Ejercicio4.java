4d36c652e79db46e820d4497245e53baead57ecc
RobinsonLatour <latourrobinson@gmail.com>
2025-03-29 12:11:43 -0300

    Primer Commit

---
+++ src/Ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese base del rectangulo en metros");
+        double base = leer.nextDouble();
+
+        System.out.println("Ingrese altura del rectangulo en metros");
+        double altura = leer.nextDouble();
+
+        System.out.println("El área del rectangulo es de: " + (base * altura) + " metros cuadrados");
+
+    }
+}
